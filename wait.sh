85b3e8b7f1229d397c885b344ceda7178235d168
PhilipKandyba <philip.kanduba@gmail.com>
2023-09-04 15:35:20 +0000

    Update wait.sh

--- wait.sh
+++ wait.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-until $(curl --output /dev/null --silent --head --fail http://selenium-hub:4444 && curl --output /dev/null --silent --head --fail http://memos:5230 ); do
-echo "Waiting for selenium hub and react memo being started"
+until $(curl --output /dev/null --silent --head --fail http://localhost:4444 && curl --output /dev/null --silent --head --fail http://localhost:5230 ); do
+echo "Waiting for selenium hub and memo being started"
 sleep 1
 done
